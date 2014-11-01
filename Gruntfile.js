c3dc0e567c0f9a9054f593c91d96988f6bb1b6ac
mfouad <m8fouad@gmail.com>
2014-11-01 12:59:16 +0200

    added wiredep to HTML to detect new dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,81 +1,273 @@
-module.exports = function (grunt) {
-    'use strict';
-    // Project configuration
-    grunt.initConfig({
-        // Metadata
-        pkg: grunt.file.readJSON('package.json'),
-        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-            ' Licensed <%= props.license %> */\n',
-        // Task configuration
-        concat: {
-            options: {
-                banner: '<%= banner %>',
-                stripBanners: true
-            },
-            dist: {
-                src: ['lib/Today.js'],
-                dest: 'dist/Today.js'
-            }
-        },
-        uglify: {
-            options: {
-                banner: '<%= banner %>'
-            },
-            dist: {
-                src: '<%= concat.dist.dest %>',
-                dest: 'dist/Today.min.js'
-            }
-        },
-        jshint: {
-            options: {
-                node: true,
-                curly: true,
-                eqeqeq: true,
-                immed: true,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                sub: true,
-                undef: true,
-                unused: true,
-                eqnull: true,
-                browser: true,
-                globals: { jQuery: true },
-                boss: true
-            },
-            gruntfile: {
-                src: 'gruntfile.js'
-            },
-            lib_test: {
-                src: ['lib/**/*.js', 'test/**/*.js']
-            }
-        },
-        qunit: {
-            files: ['test/**/*.html']
-        },
-        watch: {
-            gruntfile: {
-                files: '<%= jshint.gruntfile.src %>',
-                tasks: ['jshint:gruntfile']
-            },
-            lib_test: {
-                files: '<%= jshint.lib_test.src %>',
-                tasks: ['jshint:lib_test', 'qunit']
-            }
-        }
-    });
+/* global require */
+/* global module */
 
-    // These plugins provide necessary tasks
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-qunit');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+module.exports = function(grunt) {
+	'use strict';
 
-    // Default task
-    grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+	// Load grunt tasks automatically
+	require('load-grunt-tasks')(grunt);
+
+	// Time how long tasks take. Can help when optimizing build times
+	require('time-grunt')(grunt);
+
+	// Define the configuration for all the tasks
+	grunt.initConfig({
+
+		path: {
+			app: 'app',
+			dist: 'dist'
+		},
+		
+	
+
+		// Watches files for changes and runs tasks based on the changed files
+		watch: {
+			bower: {
+				files: ['bower.json'],
+				tasks: ['wiredep']
+			},
+			js: {
+				files: ['<%= path.app %>/js/{,*/}*.js'],
+				tasks: ['newer:jshint:all'],
+				options: {
+					livereload: '<%= connect.options.livereload %>'
+				}
+			},
+			styles: {
+				files: ['<%= path.app %>/css/{,*/}*.css'],
+				tasks: ['newer:copy:styles', 'autoprefixer']
+			},
+			gruntfile: {
+				files: ['Gruntfile.js']
+			},
+			livereload: {
+				options: {
+					livereload: '<%= connect.options.livereload %>'
+				},
+				files: ['<%= yeoman.app %>/{,*/}*.*']
+			}
+		},
+		// Empties folders to start fresh
+		clean: {
+			dist: {
+				files: [{
+					dot: true,
+					src: ['.tmp', 'dist']
+        }]
+			}
+		},
+
+		// Automatically inject Bower components into the app
+		wiredep: {
+			app: {
+				src: ['<%= path.app %>/*.html'],
+
+			}
+		},
+
+		// Reads HTML for usemin blocks to enable smart builds that automatically
+		// concat, minify and revision files. Creates configurations in memory so
+		// additional tasks can operate on them
+		useminPrepare: {
+			html: '<%= path.app %>/*.html',
+			options: {
+				dest: '<%= path.dist %>',
+				flow: {
+					html: {
+						steps: {
+							js: ['concat', 'uglifyjs'],
+							css: ['cssmin']
+						},
+						post: {}
+					}
+				}
+			}
+		},
+
+		// Performs rewrites based on useminPrepare configuration
+		usemin: {
+			html: ['<%= path.dist %>/{,*/}*.html'],
+			css: ['<%= path.dist %>/styles/{,*/}*.css'],
+			options: {
+				assetsDirs: ['<%= path.dist %>', '<%= path.dist %>/images']
+			}
+		},
+
+		// The following *-min tasks will produce minified files in the dist folder
+		// By default, your `index.html`'s <!-- Usemin block --> will take care of
+		// minification. These next options are pre-configured if you do not wish
+		// to use the Usemin blocks.
+		// cssmin: {
+		//   dist: {
+		//     files: {
+		//       '<%= path.dist %>/styles/main.css': [
+		//         '.tmp/styles/{,*/}*.css'
+		//       ]
+		//     }
+		//   }
+		// },
+		// uglify: {
+		//   dist: {
+		//     files: {
+		//       '<%= path.dist %>/scripts/scripts.js': [
+		//         '<%= path.dist %>/scripts/scripts.js'
+		//       ]
+		//     }
+		//   }
+		// },
+		// concat: {
+		//   dist: {}
+		// },
+
+		imagemin: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '<%= path.app %>/images',
+					src: '{,*/}*.{png,jpg,jpeg,gif}',
+					dest: '<%= path.dist %>/images'
+        }]
+			}
+		},
+
+		svgmin: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '<%= path.app %>/images',
+					src: '{,*/}*.svg',
+					dest: '<%= path.dist %>/images'
+        }]
+			}
+		},
+
+		htmlmin: {
+			dist: {
+				options: {
+					collapseWhitespace: true,
+					conservativeCollapse: true,
+					collapseBooleanAttributes: true,
+					removeCommentsFromCDATA: true,
+					removeOptionalTags: true
+				},
+				files: [{
+					expand: true,
+					cwd: '<%= path.dist %>',
+					src: ['*.html', 'views/{,*/}*.html'],
+					dest: '<%= path.dist %>'
+        }]
+			}
+		},
+
+		// ngmin tries to make the code safe for minification automatically by
+		// using the Angular long form for dependency injection. It doesn't work on
+		// things like resolve or inject so those have to be done manually.
+		ngmin: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '.tmp/concat/scripts',
+					src: '*.js',
+					dest: '.tmp/concat/scripts'
+        }]
+			}
+		},
+
+		// Replace Google CDN references
+		cdnify: {
+			dist: {
+				html: ['<%= path.dist %>/*.html']
+			}
+		},
+
+		// Copies remaining files to places other tasks can use
+		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					dot: true,
+					cwd: '<%= path.app %>',
+					dest: '<%= path.dist %>',
+					src: [
+						'*.{ico,png,txt}',
+						'.htaccess',
+						'*.html',
+						'views/{,*/}*.html',
+						'images/{,*/}*.{webp}',
+						'fonts/*'
+          			]
+        }, {
+					expand: true,
+					cwd: '.tmp/images',
+					dest: '<%= path.dist %>/images',
+					src: ['generated/*']
+        }, {
+					expand: true,
+					cwd: 'bower_components/bootstrap/dist',
+					src: 'fonts/*',
+					dest: '<%= path.dist %>'
+        }]
+			},
+			styles: {
+				expand: true,
+				cwd: '<%= path.app %>/styles',
+				dest: '.tmp/styles/',
+				src: '{,*/}*.css'
+			}
+		},
+
+		connect: {
+			options: {
+				port: 9000,
+				// Change this to '0.0.0.0' to access the server from outside.
+				hostname: 'localhost',
+				livereload: 35729,
+				open: false
+			},
+			livereload: {
+				options: {
+					open: true,
+					middleware: function (connect) {
+						return [
+						  connect.static('.tmp'),
+						  connect().use('/bower_components', connect.static('./bower_components')),
+						  connect.static(appConfig.app)
+						];
+					}
+				}
+			},
+			dist: {
+				options: {
+					open: true,
+					base: '<%= yeoman.dist %>'
+				}
+			}
+		},
+
+	});
+
+
+	grunt.registerTask('build', [
+    'clean:dist',
+    'wiredep',
+    'useminPrepare',
+	'copy:styles',
+	'imagemin',
+	'svgmin',
+    'concat',
+    'ngmin',
+    'copy:dist',
+    'cdnify',
+    'cssmin',
+    'uglify',
+    'usemin',
+    'htmlmin',
+	'connect:livereload',
+	'watch'
+
+  ]);
+
+	grunt.registerTask('default', [
+    'build'
+  ]);
 };
-
---
+++ xGruntfile.coffee
@@ -0,0 +1,149 @@
+module.exports = (grunt) ->
+
+  # task configurations
+  # initializing task configuration
+  grunt.initConfig
+
+    # meta data
+    pkg: grunt.file.readJSON("package.json")
+    banner: "/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - " +
+      "<%= grunt.template.today(\"yyyy-mm-dd\") %>\n" +
+      "<%= pkg.homepage ? \"* \" + pkg.homepage + \"\\n\" : \"\" %>" +
+      "* Copyright (c) <%= grunt.template.today(\"yyyy\") %> <%= pkg.author.name %>;" +
+      " Licensed <%= _.pluck(pkg.licenses, \"type\").join(\", \") %> */\n"
+
+    # files that our tasks will use
+    files:
+      html:
+        src: "app/index.html"
+
+      less:
+        src: ["app/css/*.css"]
+
+      js:
+        vendor: [
+          "bower_modules/jquery/jquery.js"
+          "bower_modules/underscore/underscore.js"
+          "bower_modules/underscore.string/lib/underscore.string.js"
+          "bower_modules/extend.js/index.js"
+          "bower_modules/backbone/backbone.js"
+          "bower_modules/backbone-fixins.js/index.js"
+          "bower_modules/backbone.stickit/backbone.stickit.js"
+          "bower_modules/base64js/base64.js"
+          "bower_modules/handlebars/handlebars.js"
+        ]
+
+        app:
+          main: "app/js/*.js"
+          compiled: "dist/js/app.min.js"
+
+    # task configuration
+    browserify:
+      app:
+        files:
+          "<%= files.js.app.compiled %>" : "<%= files.js.app.main %>"
+        options:
+          debug: true
+          transform: ["coffeeify"]
+
+    concat_sourcemap:
+      options:
+        sourcesContent: true
+      app:
+        src: [
+          "<%= files.js.vendor %>"
+          "<%= files.templates.compiled %>"
+        ]
+        dest: "generated/js/vendor.min.js"
+
+    watch:
+      options:
+        livereload: true
+
+      # targets for watch
+      html:
+        files: ["<%= files.html.src %>"]
+        tasks: ["copy"]
+
+      js:
+        files: ["<%= files.js.vendor %>"]
+        tasks: ["concat_sourcemap"]
+
+      coffee:
+        files: ["app/cjs/**/*.coffee"]
+        tasks: ["browserify", "concat_sourcemap"]
+
+      less:
+        files: ["<%= files.less.src %>"]
+        tasks: ["less:dev"]
+
+    imagemin:
+      dist:
+        files: [
+          expand: true,
+          cwd: 'app/img'
+          src: '**/*.{png,jpg,jpeg}'
+          dest: 'generated/img-min'
+        ]
+    less:
+      options:
+        ieCompat: false
+
+      dev:
+        src: "<%= files.less.src %>"
+        dest: "generated/css/style.css"
+
+      dist:
+        options:
+          cleancss: true
+          compress: true
+        src: "<%= files.less.src %>"
+        dest: "dist/css/style.css"
+
+    handlebars:
+      options:
+        namespace: "JST"
+        wrapped: true
+      compile:
+        src: "<%= files.templates.src %>"
+        dest: "<%= files.templates.compiled %>"
+
+    copy:
+      html:
+        files:
+          "generated/index.html" : "<%= files.html.src %>"
+          "dist/index.html"      : "<%= files.html.src %>"
+
+    server:
+      base: "#{process.env.SERVER_BASE || 'generated'}"
+      web:
+        port: 8000
+
+    open:
+      dev:
+        path: "http://localhost:<%= server.web.port %>"
+
+    uglify:
+      options:
+        banner: "<%= banner %>"
+
+      dist:
+        sourceMapIn: "dist/js/app.min.js.map"
+        sourceMap:   "dist/js/app.min.js.map"
+        src: "<%= concat_sourcemap.app.dest %>" # input from the concat_sourcemap process
+        dest: "dist/js/app.min.js"
+
+    clean:
+      workspaces: ["dist", "generated"]
+
+  # loading local tasks
+  grunt.loadTasks "tasks"
+
+  # loading external tasks (aka: plugins)
+  # Loads all plugins that match "grunt-", in this case all of our current plugins
+  require('matchdep').filterAll('grunt-*').forEach(grunt.loadNpmTasks)
+
+  # creating workflows
+  grunt.registerTask "default", ["handlebars", "imagemin", "less:dev", "browserify", "concat_sourcemap", "copy", "server", "open", "watch"]
+  grunt.registerTask "build", ["clean", "handlebars", "imagemin", "less:dist", "browserify", "concat_sourcemap", "uglify", "copy"]
+  grunt.registerTask "prodsim", ["build", "server", "open", "watch"]
